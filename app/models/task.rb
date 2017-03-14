882135f4d8ac9e1745f0bb8eaa69b7eb0d86d70e
Daniel Morris <dan@sofarsounds.com>
2017-03-14 13:33:21 +0000

    days elapsed

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,12 @@ class Task < ActiveRecord::Base
 	validates_presence_of :title
 	validate :future_completed_date
 
+	def days_elapsed_since_creation
+		(Time.now - self.created_at).to_i
+	end
+
 	private
+
 	def future_completed_date
     if !completed.blank? && completed > Date.today
       self.errors.add(:completed, "can't be in the future")
