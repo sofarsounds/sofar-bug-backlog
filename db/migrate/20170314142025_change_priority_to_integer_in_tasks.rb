d0103ac527b286e12cc0b686d5c3bbeade62f8a7
Daniel Morris <dan@sofarsounds.com>
2017-03-14 14:32:25 +0000

    PG cast bug
    ActiveRecord::StatementInvalid: PG::DatatypeMismatch: ERROR:  column "priority" cannot be cast automatically to type integer
    HINT:  You might need to specify "USING priority::integer".

--- db/migrate/20170314142025_change_priority_to_integer_in_tasks.rb
+++ db/migrate/20170314142025_change_priority_to_integer_in_tasks.rb
@@ -1,5 +1,5 @@
 class ChangePriorityToIntegerInTasks < ActiveRecord::Migration
   def change
-    change_column(:tasks, :priority, :integer, default: 1)
+    change_column(:tasks, :priority, 'integer USING CAST(priority AS integer)', default: 1)
   end
 end
