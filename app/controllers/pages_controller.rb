50f24c1c1d230e90ec45bb57cc995192f3d53da1
Daniel Morris <dan@sofarsounds.com>
2017-03-14 22:01:10 +0000

    calculate late and active bug counts in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,7 @@ end
 class PagesController < ApplicationController
   def home
   	@tasks = Task.order(priority: :desc, reported_count: :desc)
+    @active_tasks_count = Task.where.not(:completed).count
+    @late_tasks_count = Task.where.not(:completed).where("created_at >= ?", 10.days.ago).count
   end
 end
