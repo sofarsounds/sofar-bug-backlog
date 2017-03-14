40255f493cff00018970c6d6bdc56e0e70b0dbb1
Daniel Morris <dan@sofarsounds.com>
2017-03-14 14:26:42 +0000

    translate int priority to words in list

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,16 @@
 module TasksHelper
+
+  def readable_priority(task)
+    case task.priority
+    when 3
+      "Highest"
+    when 2
+      "High"
+    when 1
+      "Medium"
+    when 0
+      "Low"
+    end
+  end
+
 end
