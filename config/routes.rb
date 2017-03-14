66c94405a67abb1ecb827754a2c313264e1b739f
Daniel Morris <dan@sofarsounds.com>
2017-03-14 14:07:35 +0000

    BAD PRACTICE - change inc to get

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
  root to:  'pages#home'
- resources :tasks, except: [:index]
+
+ resources :tasks, except: [:index] do
+   member do
+     get :inc
+   end
+ end
+
 end
