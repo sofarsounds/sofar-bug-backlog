a1a8b8a1d0835771d31d4689c6ced3b09796ca82
Daniel Morris <dan@sofarsounds.com>
2017-03-21 15:04:37 +0000

    kiosk cuke steps

---
+++ features/step_definitions/kiosk_steps.rb
@@ -0,0 +1,19 @@
+When(/^I visit the kiosk page$/) do
+  visit kiosk_path
+end
+
+Then(/^I should not see a big header$/) do
+  expect(page).to_not have_selector :css, '.hero-unit'
+end
+
+Then(/^I should not see row \+(\d+) buttons$/) do |arg1|
+  expect(page).to_not have_selector :css, '.hero-plus'
+end
+
+Then(/^I should not see row edit buttons$/) do
+  expect(page).to_not have_selector :css, '.icon-edit'
+end
+
+Then(/^I should not see row delete buttons$/) do
+  expect(page).to_not have_selector :css, '.icon-remove'
+end
