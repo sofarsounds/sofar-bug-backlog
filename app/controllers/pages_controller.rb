9997791452f614e536efe857fb556f93b314cea2
Daniel Morris <dan@sofarsounds.com>
2017-03-22 15:07:17 +0000

    remove active bugs title
    Problematic!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
   	@tasks = Task.order(priority: :desc, reported_count: :desc)
-    @active_tasks_count = Task.where.not(:completed).count
   end
 end
